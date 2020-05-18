ef84b29ceac372fa01a1cd8e300f8e5d71afad06
Anton Liverov <liverov@yandex.ru>
2020-05-18 22:12:53 +0300

    Правка комментариев и верстка по PP главной страницы моб. версии

--- source/js/script.js
+++ source/js/script.js
@@ -17,3 +17,18 @@ Array.prototype.forEach.call(cards, function (card) {
     }
   };
 });
+
+/* TOGGLE MENU */
+var main_nav_toggle = document.querySelector(".main-nav__toggle");
+var button_open = document.querySelector(".main-nav__toggle--open");
+var main_nav_list = document.querySelector(".main-nav__list");
+var main_nav_search = document.querySelector(".main-nav__search");
+var main_nav_cart = document.querySelector(".main-nav__cart");
+
+button_open.addEventListener("click", function(e) {
+    main_nav_list.classList.toggle("hidden-block");
+    main_nav_search.classList.toggle("hidden-block");
+    main_nav_cart.classList.toggle("hidden-block");
+    main_nav_toggle.classList.toggle("main-nav__toggle--open");
+    main_nav_toggle.classList.toggle("main-nav__toggle--close");
+});
